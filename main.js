89336db3c8e4ec0a5c00b32f29a0592ad26d2cdf
박정현 <97150068+qkraksehs@users.noreply.github.com>
2022-02-22 10:46:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // main.js
+
+$(function(){
+    $('.img-list > li > a').on('click', function(e){
+        // e.preventDefault(); // 기본이벤트 제거
+        var src = $(this).children().attr('src');
+        $('.main-photo > img').attr('src', src);
+    })
+})
+// document.ready() - html이 loading된후 함수가 실행
+
+// loading 준비 완료 이벤트
+window.onload = function() {
+    // 로딩중 지연시간
+    setTimeout(function(){
+        // console.log('3초가 경과함...')
+        $('.splash-screen').removeClass('on');
+        // console.log('문서 로딩 완료!')
+    }, 3000)
+    
+    
+}
